601f07f35661e1159aa08eb4a1d7576a69cccf21
Will Daly <will.e.daly@gmail.com>
2019-10-13 12:49:45 -0700

    Export catalog meta structs in rdbms module

--- src/rdbms/mod.rs
+++ src/rdbms/mod.rs
@@ -3,6 +3,6 @@ mod datatype;
 mod error;
 mod key;
 
-pub use catalog::Catalog;
+pub use catalog::{Catalog, ColumnMeta, DatabaseMeta, SystemMeta, TableMeta};
 pub use datatype::DataType;
 pub use error::Error;
